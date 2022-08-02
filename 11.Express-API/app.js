6c7ba461af20a73b7aeb9201bd9b599335ba68b8
Riajul Islam <javascriptforeverything@gmail.com>
2022-08-02 23:39:54 +0600

    add api security middleware

--- 11.Express-API/app.js
+++ 11.Express-API/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 
+const helmet = require('helmet') 										// Add some secrirty option in response heading
+const rateLimit = require('express-rate-limit') 		// To limit request per hour from a single host.
+const compression = require('compression') 					// To compress http response that send to client
+const xssClean = require('xss-clean') 							// To Remove <tags> entities, so that .js use as string not script
+
 const userRouter = require('./routes/userRoute')
 const productRouter = require('./routes/productRoute')
 const reviewRouter = require('./routes/reviewRoute')
@@ -9,6 +14,14 @@ const errorController = require('./controllers/errorController')
 
 const app = express()
 
+app.use( helmet() )
+app.use( compression() )
+app.use('/api', rateLimit({
+	max: 100,
+	windowMs: 1000*60*60, 			// 1 Hour
+	message: 'reached max limit of 100 request per hour'
+}))
+app.use( xssClean() )
 
 //----------[ Middleware Section ]----------
 
