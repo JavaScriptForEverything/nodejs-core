ff4f3155a638d0e5a62a1718b8a955a32c335bd1
Riajul Islam <javascriptforeverything@gmail.com>
2022-08-05 19:03:49 +0600

    enable global error handling in express

--- 06.HTTP/express.js
+++ 06.HTTP/express.js
@@ -6,15 +6,37 @@ const app = express()
 app.use(express.static('./public')) 			// server static html pages or files
 app.use(express.json()) 									// allow to parse json data send to server
 
-// GET 	/ 				: load /public/index.html
-// GET 	/api 			: get json data
+// // GET 	/ 				: load /public/index.html
+// // GET 	/api 			: get json data
 
-app.get('/api', (req, res) => {
+// app.get('/api', (req, res) => {
+// 	res.status(200).json({
+// 		status: 'success',
+// 		message: 'Hello world'
+// 	})
+// })
+
+
+const catchAsync = (fn) => (req, res, next) => fn(req, res, next).catch(next)
+
+app.get('/api', catchAsync( (req, res) => {
+	return Promise.reject('Hello')
+
 	res.status(200).json({
 		status: 'success',
 		message: 'Hello world'
 	})
+}))
+
+app.use('*', (err, req, res, next) => {
+	res.status(400).json({
+		status: 'error',
+		message: err.message
+	})
 })
+
+
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
